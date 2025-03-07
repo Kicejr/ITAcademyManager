edab04e19205e2a802d74d91ba2e42ff737bc095
Kicejr <Kicejr777@gmail.com>
2025-03-07 01:15:12 +0000

    Ajout de new fichiers

---
+++ views/formation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nos formations</title>
+</head>
+<body>
+    <section class="packages">
+        <h1 class="heading-title">Nos Formations</h1>
+
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="images/" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
